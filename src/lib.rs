945efe57d651bbfaffa852ec57f30eb7115b8564
Noah Weninger <nweninge@ualberta.ca>
2016-02-28 09:27:23 -0700

    new plugin: amplitude modulus

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod binflipper;
 mod pitchshifter;
 mod centroid;
 mod expavg;
+mod modularamp;
 
 #[no_mangle]
 pub fn get_ladspa_descriptor(index: u64) -> Option<PluginDescriptor> {
@@ -16,6 +17,7 @@ pub fn get_ladspa_descriptor(index: u64) -> Option<PluginDescriptor> {
         1 => Some(pitchshifter::get_descriptor()),
         2 => Some(centroid::get_descriptor()),
         3 => Some(expavg::get_descriptor()),
+        4 => Some(modularamp::get_descriptor()),
         _ => None,
     }
 }
---
+++ src/modularamp.rs
@@ -0,0 +1,85 @@
+use pvoc::{PhaseVocoder, Bin};
+use ladspa::{self, PluginDescriptor, PortDescriptor, Port, DefaultValue, Plugin, PortConnection};
+
+fn fmod(a: f64, b: f64) -> f64 {
+    a - b * (a / b).floor()
+}
+
+struct ModularAmp {
+    pvoc: PhaseVocoder,
+}
+
+impl ModularAmp {
+    fn new(_: &PluginDescriptor, sample_rate: u64) -> Box<Plugin + Send> {
+        Box::new(ModularAmp { pvoc: PhaseVocoder::new(2, sample_rate as f64, 10, 32) })
+    }
+
+    fn process(factor: f64,
+               channels: usize,
+               bins: usize,
+               input: &[Vec<Bin>],
+               output: &mut [Vec<Bin>]) {
+        for i in 0..channels {
+            for j in 0..bins {
+                output[i][j].freq = input[i][j].freq;
+                output[i][j].amp = fmod(input[i][j].amp, factor);
+            }
+        }
+    }
+}
+
+impl Plugin for ModularAmp {
+    fn run<'a>(&mut self, _: usize, ports: &[&'a PortConnection<'a>]) {
+        let input = vec![ports[0].unwrap_audio(), ports[1].unwrap_audio()];
+        let mut outputl = ports[2].unwrap_audio_mut();
+        let mut outputr = ports[3].unwrap_audio_mut();
+        let mut output = vec![&mut outputl[..], &mut outputr[..]];
+        let factor = *ports[4].unwrap_control() as f64;
+        self.pvoc.process(&input, &mut output, |channels: usize,
+                           bins: usize,
+                           input: &[Vec<Bin>],
+                           output: &mut [Vec<Bin>]| {
+            ModularAmp::process(factor, channels, bins, input, output)
+        });
+    }
+}
+
+pub fn get_descriptor() -> PluginDescriptor {
+    PluginDescriptor {
+        unique_id: 9404,
+        label: "pvoc_modamp",
+        properties: ladspa::PROP_NONE,
+        name: "pvoc amplitude modulus",
+        maker: "Noah Weninger",
+        copyright: "None",
+        ports: vec![Port {
+                        name: "Left Audio In",
+                        desc: PortDescriptor::AudioInput,
+                        ..Default::default()
+                    },
+                    Port {
+                        name: "Right Audio In",
+                        desc: PortDescriptor::AudioInput,
+                        ..Default::default()
+                    },
+                    Port {
+                        name: "Left Audio Out",
+                        desc: PortDescriptor::AudioOutput,
+                        ..Default::default()
+                    },
+                    Port {
+                        name: "Right Audio Out",
+                        desc: PortDescriptor::AudioOutput,
+                        ..Default::default()
+                    },
+                    Port {
+                        name: "Mod",
+                        desc: PortDescriptor::ControlInput,
+                        hint: None,
+                        default: Some(DefaultValue::Value1),
+                        lower_bound: Some(0.0),
+                        upper_bound: Some(25.0),
+                    }],
+        new: ModularAmp::new,
+    }
+}
